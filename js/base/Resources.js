570da42a372e382362704237aad38b771a96f2ac
tst <tianshenti@qq.com>
2019-06-12 18:40:34 +0800

    teacher-v1.0

---
+++ js/base/Resources.js
@@ -0,0 +1,9 @@
+// 图片资源数组
+export const Resources = [
+  ['background', 'res/background.png'],
+  ['birds', 'res/birds.png'],
+  ['land', 'res/land.png'],
+  ['pieDown', 'res/pie_down.png'],
+  ['pieUp', 'res/pie_up.png'],
+  ['startButton', 'res/start_button.png']
+];
